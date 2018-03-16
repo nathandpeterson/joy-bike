446493409906c9057ccf56b6b3bcab4937499e1f
Nathan Daniel Peterson <natperson@gmail.com>
2018-03-16 14:21:26 -0700

    add placeholder icon

--- src/components/Main/MainImage/index.js
+++ src/components/Main/MainImage/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+const noSelection = () => (
+    <div>
+        <div className='bike-icon'>
+            <i class="fas fa-bicycle"></i>
+        </div>
+        <h5>Just pick an ebike to get started</h5>
+    </div> 
+)
+
 const MainImage = ({ selectedBike }) => {
-    if (!selectedBike.img_url) return <h5>Just pick an ebike to get started</h5>
+    if (!selectedBike.img_url) return noSelection()
     return (
     <div className='main-image-container'>
         <img src={ selectedBike.img_url } 
